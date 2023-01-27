20b966aacafa46033f7013f48d46789c0b81059f
Nicolas-Fabian-Sanchez <nicosanchez10@hotmail.com>
2023-01-26 23:02:36 -0300

    ultimo cambio

--- interzas.py
+++ interzas.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+import tkinter.font as tkFont
 from random import randint
 from funcionBaseDatos import baseDatos , listapalabras,obtenerUsuario ,editarPuntaje,agregarUsuario,tablaPosiciones
 
@@ -17,24 +18,30 @@ root.config(bg="lightblue") # establezco el color de fondo de la ventana
 #----------- creacion de las funciones para la presentacion --------------------
 
 def bienvenida(event=None):
-  global nombre
+  
+  global nombre , saludo
   nombre = etiqueta_1.get()
+  nombre= nombre.capitalize()
   frame.place_forget()
   frame_2.place_forget()
   etiqueta_1.place_forget()
+  saludo = ttk.Label(root,text= f"""
+           ¡¡¡Bienvenido {nombre} !!! al juego del "AHORCADO"
+            ¡La cosa es asi yo pienso una palabra y vos 
+            tratas de adivinarla "Para eso tenes 6 vidas" !  
+                      """, font="helvetica 18 ", background="lightblue" )
   saludo.place(x= 30 ,y = 30 )
   boton_comenzar.place(x= 280, y=180 , width=140 , height= 60)
     
 def comenzar (event=None):
-  global nombre
+  #global nombre
   boton_comenzar.place_forget ()
   saludo.place_forget () 
   etiqueta_3.place_forget()
   botonSi.place_forget()
   botonNo.place_forget()
   tabla_1.place_forget()
-  #tabla_2.place_forget() #--- aca esta el error , pienso que no lo puedo anular lista " no se  ver"
-  #tabla_3.place_forget() #--- si no lo anulo queda mal
+  
   presentacioPalabraSecreta.place(x=20,y=20)
   palabraSecreta.place(x= 20 , y=60)
   elijaLetra.place(x=20 , y = 120)
@@ -48,7 +55,8 @@ def comenzar (event=None):
   etiquetaResultado.config(text="")
   
  #creo las variables del funcionamiento del programa
-  global palabrasSecretas, letrasIntentadas,vidasRestantes,puntaje, nombre
+  
+  global palabrasSecretas, letrasIntentadas,vidasRestantes,puntaje
   indice = randint(0,len(listaPalabras)-1)
   palabrasSecretas = listaPalabras[indice]
   letrasIntentadas=[] #creo una lista vacia para ingresar aqui las letras intentadas
@@ -60,7 +68,7 @@ def comenzar (event=None):
   
 #---------------- creacion funciones del funcionamiento programa ---------------
 def eleccionLetra():
-  global vidasRestantes, puntaje
+  global  puntaje,vidasRestantes
   letraIngresada = ingresoLetra.get() #le doy a la varieble el valor que ingreso en el input
   # Chequear si esta letra esta en letras intentadas
   if letraIngresada in letrasIntentadas: # ['a']
@@ -77,8 +85,8 @@ def eleccionLetra():
   if vidasRestantes == 0:
     #etiquetaResultado.config(text=f"""{nombre} Perdiste, la palabra 
     #era '{palabrasSecretas}'""")
-    puntaje = -10
-    finalizar()
+     puntaje = -10
+     finalizar()
   
   mostrarPalabra()
   ingresoLetra.delete(0)
@@ -115,20 +123,20 @@ def mostrarPalabra():
   etiquetaVidas.config(text=f"{nombre} te restan {vidasRestantes} intentos", font='bold 12')
   
 def posiciones():
-  global tabla_1,tabla_2,tabla_3,usuarios
+  global tabla_2,tabla_3,usuarios
   usuarios = tablaPosiciones()
   fila = 70
   tabla_1.place(x=280, y=fila)
   fila=fila+30 # esto lo hago para que cuando itere me coloque uno debajo del otro a la misma linea
   for usu in usuarios:
     tabla_2=ttk.Label(
-      text=f"{usu['nombre']}",
+       text=f"{usu['nombre']}",
       # width=60,
       justify='right',background="lightblue"
     ).place(x=300,y=fila)
     tabla_3=tk.Label(
-      text=f"{usu['puntaje']}",
-      width=3,background="lightblue"
+     text=f"{usu['puntaje']}",
+     width=3,background="lightblue"
     ).place(x=370,y=fila)
     fila = fila + 20 
  
@@ -147,7 +155,7 @@ def finalizar():
   
   
   # Gestion del usuario
-  nombre = nombre.lower()
+  #nombre = nombre.lower()
   usuario = obtenerUsuario(nombre)
 
   if usuario:
@@ -176,8 +184,14 @@ def finalizar():
 def cerrarPrograma():
   root.destroy()
 
-def reiniciar ():
-  pass
+def reiniciar():
+  tabla_2.remove() #--- aca esta el error , pienso que no lo puedo anular lista " no se  ver"
+  tabla_3.remove() #--- si no lo anulo queda mal
+  comenzar()
+  
+
+
+  
 
 #------------- creacion de las etiquetas presentacion (widgets en ingles) ---------- 
 frame = ttk.Label(root,text="Ingrese su nombre aqui : ", width="25") # de esta forma ingreso un texto a la ventana
@@ -185,16 +199,17 @@ frame = ttk.Label(root,text="Ingrese su nombre aqui : ", width="25") # de esta f
 frame.place(x=260 ,y=10) # place va si o si para que el elemento se muestre en la ventana y dentro de el configuro la po
 etiqueta_1 = ttk.Entry(root, font="helvetica 20")# de esta forma creo un input para ingresar datos del usuario
 etiqueta_1.place(x=185 , y= 35)
-nombre = etiqueta_1.get() # con get tomo el valor de lo que pose adentro la etiqueta
+
+#nombre =  "" #etiqueta_1.get() con get tomo el valor de lo que pose adentro la etiqueta
 #nombre= nombre.lower()
 frame_2 = ttk.Button(root,text="Entrar!!!",command= bienvenida )#creo un boton de entrada para que se realce la funcion 
 frame_2.place(x=295, y=95)
 boton_comenzar =ttk.Button(root,text="Comencemos !!!",command=comenzar)
-saludo = ttk.Label(root,text= f"""
-            ¡¡¡Bienvenido {nombre} !!! al juego del "AHORCADO"
-            ¡La cosa es asi yo pienso una palabra y vos 
-            tratas de adivinarla "Para eso tenes 6 vidas" !  
-                      """, font="helvetica 18 ", background="lightblue" )
+#saludo = ttk.Label(root,text= f"""
+           #¡¡¡Bienvenido {nombre} !!! al juego del "AHORCADO"
+           #¡La cosa es asi yo pienso una palabra y vos 
+            #tratas de adivinarla "Para eso tenes 6 vidas" !  
+                      #""", font="helvetica 18 ", background="lightblue" )
 
 
 #---------------- creacion de etiquetas juego ---------------
@@ -211,13 +226,13 @@ imagen= tk.PhotoImage(file="ahorcado.png")
 etiquetaImagen=ttk.Label(root,image=imagen)
 etiquetaResultado = ttk.Label(root,text="",background="lightblue",font="helvetica 20")
 etiqueta_3=ttk.Label(root,text="Desea seguir jugando ?",background="lightblue",font="helvetica 20")
-botonSi = ttk.Button(root,text="SI",command=comenzar)
+botonSi = ttk.Button(root,text="SI",command=reiniciar)
 botonNo=ttk.Button(root,text="NO",command= cerrarPrograma)
 tabla_1=ttk.Label(text='Posiciones', font='Bold 20',background="lightblue")
-tabla_2=ttk.Label(root,text="")
-tabla_3=ttk.Label(root,text="")
+tabla_2=[]
+tabla_3=[]
 
-
+#tababla_posiciones=ttk.Treeview(root,columns=("Nombre","Puntos"))
 
 
     
